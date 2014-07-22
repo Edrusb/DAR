19fade0b03a97f6606b328386eafbd643c1acdf3
Denis Corbin <dar.linux@free.fr>
2014-07-22 19:09:06 +0200

    fixed typo

--- src/libdar/tools.hpp
+++ src/libdar/tools.hpp
@@ -799,7 +799,7 @@ namespace libdar
 
     extern std::string tools_get_compression_ratio(const infinint & storage_size, const infinint & file_size, bool compressed);
 
-	/// wrapper routint to strerror_r
+	/// wrapper routine to strerror_r
     extern std::string tools_strerror_r(int errnum);
 
 #ifdef GPGME_SUPPORT
