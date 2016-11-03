Edrusb/DAR

commit efb02698a6a99747a89b7099bc5e7d5d4bd2f114

fixed bug met when openning fichier_libcurl in write mode without overwriting
positionning the current_position field as the filesize to not hit error message
reporting skipping is not possible in write mode for fichier_libcurl

src/libdar/fichier_libcurl.cpp
@@ -132,6 +132,8 @@ namespace libdar
 		throw Erange("entrepot_libcur::handle_reset", string(gettext("multihandle initialization failed")));
 	    switch_to_metadata(false);
 	    add_easy_to_multi();
+	    if(append_write)
+		current_offset = get_size();
 	}
 	catch(...)
 	{

commit 713501ff48ba90f796f94ebce5014ba1835afb18

fixing bug introduced by commit efb02698a6a99747a89b7099bc5e7d5d4bd2f114

src/libdar/fichier_libcurl.cpp
@@ -132,7 +132,7 @@ namespace libdar
 		throw Erange("entrepot_libcur::handle_reset", string(gettext("multihandle initialization failed")));
 	    switch_to_metadata(false);
 	    add_easy_to_multi();
-	    if(append_write)
+	    if(append_write && m != gf_read_only)
 		current_offset = get_size();
 	}
 	catch(...)