Edrusb/DAR

commit 9f0ae13dca8c48b997b739aaa795317d97ebeb11

adding check to verify that libcurl has support for the network protocol requested to reach the remote repository

src/libdar/entrepot_libcurl.cpp
@@ -81,6 +81,13 @@ namespace libdar
 	set_user_ownership(""); // not used for this type of entrepot
 	set_group_ownership(""); // not used for this type of entrepot
 
+	if(!curl_is_protocol_available(proto))
+	{
+	    string named_proto = curl_protocol2string(proto);
+	    throw Erange("entrepot_libcurl::entrepot_libcurl",
+			 tools_printf(gettext("protocol %S is not supported by libcurl, aborting"), & named_proto));
+	}
+
 	easyhandle = curl_easy_init();
 	if(easyhandle == nullptr)
 	    throw Erange("entrepot_libcurl::entrepot_libcurl", string(gettext("Error met while creating a libcurl handle")));
@@ -634,6 +641,25 @@ namespace libdar
 		++ptr;
 	    }
 	return size*nmemb;
+    }
+
+    bool entrepot_libcurl::curl_is_protocol_available(curl_protocol proto)
+    {
+	curl_version_info_data *data = curl_version_info(CURLVERSION_NOW);
+	const char **ptr = nullptr;
+	string named_proto = curl_protocol2string(proto);
+
+	if(data == nullptr)
+	    throw SRC_BUG;
+
+	ptr = const_cast<const char **>(data->protocols);
+	if(ptr == nullptr)
+	    throw SRC_BUG;
+
+	while(*ptr != nullptr && strcmp(*ptr, named_proto.c_str()) != 0)
+	    ++ptr;
+
+	return *ptr != nullptr;
     }
 
 } // end of namespace

src/libdar/entrepot_libcurl.hpp
@@ -121,6 +121,7 @@ namespace libdar
 	static std::string build_url_from(curl_protocol proto, const std::string & host, const std::string & port);
 	static size_t get_ftp_listing_callback(void *buffer, size_t size, size_t nmemb, void *userp);
 	static size_t null_callback(void *buffer, size_t size, size_t nmemb, void *userp) { return size*nmemb; };
+	static bool curl_is_protocol_available(curl_protocol proto);
     };
 
 	/// @}

commit 5767a720b885843571305ee3f178143ce7ccfc4c

fixing bug introduced by commit 9f0ae13dca8c48b997b739aaa795317d97ebeb11

src/libdar/entrepot_libcurl.cpp
@@ -585,19 +585,19 @@ namespace libdar
 	switch(proto)
 	{
 	case proto_ftp:
-	    ret = "ftp://";
+	    ret = "ftp";
 	    break;
 	case proto_http:
-	    ret = "http://";
+	    ret = "http";
 	    break;
 	case proto_https:
-	    ret = "https://";
+	    ret = "https";
 	    break;
 	case proto_scp:
-	    ret = "scp://";
+	    ret = "scp";
 	    break;
 	case proto_sftp:
-	    ret = "sftp://";
+	    ret = "sftp";
 	    break;
 	default:
 	    throw SRC_BUG;
@@ -608,7 +608,7 @@ namespace libdar
 
     string entrepot_libcurl::build_url_from(curl_protocol proto, const string & host, const string & port)
     {
-	string ret = curl_protocol2string(proto) + host;
+	string ret = curl_protocol2string(proto) + "://" + host;
 
 	if(!port.empty())
 	    ret += ":" + port;