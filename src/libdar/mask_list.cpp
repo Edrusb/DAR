e6685da7678e2bde122556ac1358aeb26fa9a4d6
Denis Corbin <dar.linux@free.fr>
2017-07-29 11:32:37 +0200

    - improved error message issued when a file listing (-[ or -] option) is   missing for it provides the missing filename in the error message

--- src/libdar/mask_list.cpp
+++ src/libdar/mask_list.cpp
@@ -60,158 +60,166 @@ namespace libdar
 	NLS_SWAP_IN;
 	try
 	{
-	    case_s = case_sensit;              //< object's field
-	    including = include;               //< object's field
-	    fichier_local source = filename_list_st; //< where we read data from
-	    char *buffer = nullptr;               //< hold the just read data
-	    static const U_I buf_size = 20480; //< size of buffer: we read at most this number of bytes at a time
-	    list <string> tmp;                 //< list of all raw lines read, without any prefix
-	    U_I lu = 0, curs;                  //< cursor used as cisors to split data in line
-	    char *beg = nullptr;                  //< points to the beginning of the next line inside buffer, when more than one line can be found in buffer
-	    string str_beg;                    //< holds the std::string copy of beg, eventually uppercased
-	    string current_entry = "";         //< holds the current line converted to string between each read()
-	    path prefix = prefix_t;            //< the prefix to add to relative paths
-
-
-
-		/////////////
-		// changing the prefix to uppercase if case sensitivity is disabled
-
-	    if(!case_sensit)
-	    {
-		string ptp = prefix_t.display();
-		string upper;
-		tools_to_upper(ptp, upper);
-		prefix = path(upper);
-	    }
-
-
-
-		/////////////
-		// building buffer that will be used to split read data line by line
-
-	    meta_new(buffer, buf_size+1); // one char more to be able to add a '\0' if necessary
-	    if(buffer == nullptr)
-		throw Erange("mask_list::mask_list", tools_printf(gettext("Cannot allocate memory for buffer while reading %S"), &filename_list_st));
-
-
-
-		/////////////
-		// filling 'tmp' with with each line read
-
 	    try
 	    {
-		do
+		case_s = case_sensit;              //< object's field
+		including = include;               //< object's field
+		fichier_local source = filename_list_st; //< where we read data from
+		char *buffer = nullptr;               //< hold the just read data
+		static const U_I buf_size = 20480; //< size of buffer: we read at most this number of bytes at a time
+		list <string> tmp;                 //< list of all raw lines read, without any prefix
+		U_I lu = 0, curs;                  //< cursor used as cisors to split data in line
+		char *beg = nullptr;                  //< points to the beginning of the next line inside buffer, when more than one line can be found in buffer
+		string str_beg;                    //< holds the std::string copy of beg, eventually uppercased
+		string current_entry = "";         //< holds the current line converted to string between each read()
+		path prefix = prefix_t;            //< the prefix to add to relative paths
+
+
+
+		    /////////////
+		    // changing the prefix to uppercase if case sensitivity is disabled
+
+		if(!case_sensit)
 		{
-		    lu = source.read(buffer, buf_size);
+		    string ptp = prefix_t.display();
+		    string upper;
+		    tools_to_upper(ptp, upper);
+		    prefix = path(upper);
+		}
 
-		    if(lu > 0)
+
+
+		    /////////////
+		    // building buffer that will be used to split read data line by line
+
+		meta_new(buffer, buf_size+1); // one char more to be able to add a '\0' if necessary
+		if(buffer == nullptr)
+		    throw Erange("mask_list::mask_list", tools_printf(gettext("Cannot allocate memory for buffer while reading %S"), &filename_list_st));
+
+
+
+		    /////////////
+		    // filling 'tmp' with with each line read
+
+		try
+		{
+		    do
 		    {
-			curs = 0;
-			beg = buffer;
+			lu = source.read(buffer, buf_size);
 
-			do
+			if(lu > 0)
 			{
-			    while(curs < lu && buffer[curs] != '\n' && buffer[curs] != '\0')
-				curs++;
+			    curs = 0;
+			    beg = buffer;
 
-			    if(curs < lu)
+			    do
 			    {
-				if(buffer[curs] == '\0')
-				    throw Erange("mask_list::mask_list", tools_printf(gettext("Found '\0' character in %S, not a plain file, aborting"), &filename_list_st));
-				if(buffer[curs] == '\n')
+				while(curs < lu && buffer[curs] != '\n' && buffer[curs] != '\0')
+				    curs++;
+
+				if(curs < lu)
 				{
-				    buffer[curs] = '\0';
+				    if(buffer[curs] == '\0')
+					throw Erange("mask_list::mask_list", tools_printf(gettext("Found '\0' character in %S, not a plain file, aborting"), &filename_list_st));
+				    if(buffer[curs] == '\n')
+				    {
+					buffer[curs] = '\0';
+					if(!case_s)
+					    tools_to_upper(beg, str_beg);
+					else
+					    str_beg = string(beg);
+					current_entry += str_beg;
+					if(current_entry != "")
+					    tmp.push_back(current_entry);
+					current_entry = "";
+					curs++;
+					beg = buffer + curs;
+				    }
+				    else
+					throw SRC_BUG;
+				}
+				else // reached end of buffer without having found an end of string
+				{
+				    if(lu == buf_size)
+					throw Erange("mask_list::mask_list",
+						     tools_printf(gettext("line exceeding the maximum of %d characters in listing file %S, aborting"),
+								  buf_size - 1,
+								  &filename_list_st));
+				    buffer[lu] = '\0';
 				    if(!case_s)
 					tools_to_upper(beg, str_beg);
 				    else
 					str_beg = string(beg);
 				    current_entry += str_beg;
-				    if(current_entry != "")
-					tmp.push_back(current_entry);
-				    current_entry = "";
-				    curs++;
-				    beg = buffer + curs;
 				}
-				else
-				    throw SRC_BUG;
 			    }
-			    else // reached end of buffer without having found an end of string
+			    while(curs < lu);
+			}
+		    }
+		    while(lu > 0);
+
+		    if(current_entry != "")
+			tmp.push_back(current_entry);
+		}
+		catch(...)
+		{
+		    meta_delete(buffer);
+		    throw;
+		}
+		meta_delete(buffer);
+		buffer = nullptr;
+
+
+		    /////////////
+		    // completing relative paths of the list
+
+		if(prefix.is_relative() && !prefix.is_subdir_of("<ROOT>", true))
+		    throw Erange("mask_list::mask_list", gettext("Mask_list's prefix must be an absolute path or start with \"<ROOT>\" string for archive merging"));
+		else
+		{
+		    path current = "/";
+		    list <string>::iterator it = tmp.begin();
+
+		    while(it != tmp.end())
+		    {
+			try
+			{
+			    current = *it;
+			    if(current.is_relative())
 			    {
-				if(lu == buf_size)
-				    throw Erange("mask_list::mask_list",
-						 tools_printf(gettext("line exceeding the maximum of %d characters in listing file %S, aborting"),
-							      buf_size - 1,
-							      &filename_list_st));
-				buffer[lu] = '\0';
-				if(!case_s)
-				    tools_to_upper(beg, str_beg);
-				else
-				    str_beg = string(beg);
-				current_entry += str_beg;
+				current = prefix + current;
+				*it = current.display();
 			    }
 			}
-			while(curs < lu);
+			catch(Egeneric & e)
+			{
+			    string err = e.get_message();
+			    string line = *it;
+
+			    throw Erange("mask_list::mask_list", tools_printf(gettext("Error met while reading line\n\t%S\n from file %S: %S"), &line, &filename_list_st, &err));
+			}
+			it++;
 		    }
 		}
-		while(lu > 0);
 
-		if(current_entry != "")
-		    tmp.push_back(current_entry);
+		    /////////////
+		    // sorting the list of entry
+
+		    // sorting the list with a modified lexicographical order where the / as is lowest character, other letter order unchanged
+		tmp.sort(&modified_lexicalorder_a_lessthan_b);
+		tmp.unique(); // remove duplicates
+
+		    // converting the sorted list to vector, to get the indexing feature of this type
+		contenu.assign(tmp.begin(), tmp.end());
+		taille = contenu.size();
+		if(taille < contenu.size())
+		    throw Erange("mask_list::mask_list", tools_printf(gettext("Too much line in file %S (integer overflow)"), &filename_list_st));
 	    }
-	    catch(...)
+	    catch(Egeneric & e)
 	    {
-		meta_delete(buffer);
+		e.prepend_message(tools_printf(gettext("Error met while opening %S: "), &filename_list_st));
 		throw;
 	    }
-	    meta_delete(buffer);
-	    buffer = nullptr;
-
-
-		/////////////
-		// completing relative paths of the list
-
-	    if(prefix.is_relative() && !prefix.is_subdir_of("<ROOT>", true))
-		throw Erange("mask_list::mask_list", gettext("Mask_list's prefix must be an absolute path or start with \"<ROOT>\" string for archive merging"));
-	    else
-	    {
-		path current = "/";
-		list <string>::iterator it = tmp.begin();
-
-		while(it != tmp.end())
-		{
-		    try
-		    {
-			current = *it;
-			if(current.is_relative())
-			{
-			    current = prefix + current;
-			    *it = current.display();
-			}
-		    }
-		    catch(Egeneric & e)
-		    {
-			string err = e.get_message();
-			string line = *it;
-
-			throw Erange("mask_list::mask_list", tools_printf(gettext("Error met while reading line\n\t%S\n from file %S: %S"), &line, &filename_list_st, &err));
-		    }
-		    it++;
-		}
-	    }
-
-		/////////////
-		// sorting the list of entry
-
-		// sorting the list with a modified lexicographical order where the / as is lowest character, other letter order unchanged
-	    tmp.sort(&modified_lexicalorder_a_lessthan_b);
-	    tmp.unique(); // remove duplicates
-
-		// converting the sorted list to vector, to get the indexing feature of this type
-	    contenu.assign(tmp.begin(), tmp.end());
-	    taille = contenu.size();
-	    if(taille < contenu.size())
-		throw Erange("mask_list::mask_list", tools_printf(gettext("Too much line in file %S (integer overflow)"), &filename_list_st));
 	}
 	catch(...)
 	{
