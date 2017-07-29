Edrusb/DAR

commit dd600187612425ef82465ded5865fe4c1eb4e6dc

better handling of line maximum length inside a listing file
- documenting in man page the limitation of -[ and -] options concerning
  the maximum line length that could be used in a listing file. This
  limitation was only described in doc/Limitations.html
- dar now aborts if a line exceeding 20479 bytes is met in a listing file

src/libdar/mask_list.cpp
@@ -138,6 +138,11 @@ namespace libdar
 			    }
 			    else // reached end of buffer without having found an end of string
 			    {
+				if(lu == buf_size)
+				    throw Erange("mask_list::mask_list",
+						 tools_printf(gettext("line exceeding the maximum of %d characters in listing file %S, aborting"),
+							      buf_size - 1,
+							      &filename_list_st));
 				buffer[lu] = '\0';
 				if(!case_s)
 				    tools_to_upper(beg, str_beg);

doc/Limitations.html
@@ -48,8 +48,8 @@ more. This
 limit should be high enough to not be a problem. <br>
           </li>
           <li>when using a listing file to define which file to operate
-on, each line of the listing file must not be longer than 20480 bytes
-else a new line is considered after the 20480th byte. </li>
+on (-[ and -] option when using dar), each line of the listing file must not be longer than 20479 bytes
+else a new line is considered at the 20480th byte. </li>
         </ul>
       </ul>
       <br>

man/dar.1
@@ -290,7 +290,7 @@ Files or directory to only take in account, as opposed to -P. -g may be present 
 -g option cannot receive wildcards, these would not be interpreted.
 .TP 20
 -[, --include-from-file <listing_file>
-Files listed in the listing file are included for the operation. No wildcard expression is interpreted in the listing file, the null character is not allowed and the carriage return is used to separate file names (one file name per line). Note that this option applies to any files and directory exactly as -g does, with an important difference however: -g option only uses relative paths to the root directory (the directory given with the -R option), while -[ can use absolute path as well. Another difference is when the argument is a directory -g will include all the subdirectories under that directory, while when the same entry is found in a listing file given to -[ only that directory will be included, no subdirectory or subfile would be enrolled in the backup, with -[ you need to list the exact set of file you want to backup. You can thus generate a listing file with the 'find / -print > somefile' command and give 'somefile' as argument to -[ option. Note that however, dar will never save files out of the -R given root directory tree, even if some are listed in the 'somefile' file.
+Files listed in the listing file are included for the operation. No wildcard expression is interpreted in the listing file, the null character is not allowed and the carriage return is used to separate file names (one file name per line) each line must not exceed 20479 bytes. Note that this option applies to any files and directory exactly as -g does, with an important difference however: -g option only uses relative paths to the root directory (the directory given with the -R option), while -[ can use absolute path as well. Another difference is when the argument is a directory -g will include all the subdirectories under that directory, while when the same entry is found in a listing file given to -[ only that directory will be included, no subdirectory or subfile would be enrolled in the backup, with -[ you need to list the exact set of file you want to backup. You can thus generate a listing file with the 'find / -print > somefile' command and give 'somefile' as argument to -[ option. Note that however, dar will never save files out of the -R given root directory tree, even if some are listed in the 'somefile' file.
 .TP 20
 -], --exclude-from-file <listing_file>
 Files listed in the listing file are excluded from the operation. If a directory is listed in the file all its contents is excluded. This option is the opposite of -[ and acts the same was as -P option does (in particular it is compared to the whole path+filename and applies to files and directories). As for -[ option, -] listing file can contain absolute paths, but wildcards are not expanded, neither.

commit 642face9f8261d59ecec622d2d71b346d0fb613f

fixing bug introduced by commit dd600187612425ef82465ded5865fe4c1eb4e6dc

src/libdar/mask_list.cpp
@@ -138,9 +138,9 @@ namespace libdar
 				    else
 					throw SRC_BUG;
 				}
-				else // reached end of buffer without having found an end of string
+				else // reached end of buffer
 				{
-				    if(lu == buf_size)
+				    if(lu == buf_size && beg == buffer)
 				    {
 					buffer[buf_size - 1] = '\0';
 					throw Erange("mask_list::mask_list",