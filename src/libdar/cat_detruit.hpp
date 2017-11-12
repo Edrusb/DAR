7c092375f72de5b380eb1e2b335810454e4cc184
Denis Corbin <dar.linux@free.fr>
2017-11-12 11:07:52 +0100

    adding move operator and constructor to class cat_detruit

--- src/libdar/cat_detruit.hpp
+++ src/libdar/cat_detruit.hpp
@@ -50,7 +50,10 @@ namespace libdar
         cat_detruit(const std::string & name, unsigned char firm, const datetime & date) : cat_nomme(name) , del_date(date) { signe = firm; };
         cat_detruit(const smart_pointer<pile_descriptor> & pdesc, const archive_version & reading_ver, bool small);
 	cat_detruit(const cat_nomme & ref) : cat_nomme(ref.get_name()), del_date(0) { signe = ref.signature(); };
+	cat_detruit(const cat_detruit & ref) = default;
+	cat_detruit(cat_detruit && ref) = default;
 	cat_detruit & operator = (const cat_detruit & ref) = default;
+	cat_detruit & operator = (cat_detruit && ref) = default;
 	~cat_detruit() = default;
 
 	virtual bool operator == (const cat_entree & ref) const override;
