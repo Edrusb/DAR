15f6190d001766812494d584a4fe338901922ef6
Denis Corbin <dar.linux@free.fr>
2017-11-24 20:57:25 +0100

    setting the move copy and constructor of class erreurs_ext as noexcept

--- src/libdar/erreurs_ext.cpp
+++ src/libdar/erreurs_ext.cpp
@@ -36,7 +36,7 @@ namespace libdar
 	    throw Ememory("Ethread_cancel_with_attr::Ethread_cancel_with_attr");
     }
 
-    Ethread_cancel_with_attr & Ethread_cancel_with_attr::operator = (Ethread_cancel_with_attr && ref)
+    Ethread_cancel_with_attr & Ethread_cancel_with_attr::operator = (Ethread_cancel_with_attr && ref) noexcept
     {
 	Ethread_cancel::operator = (move(ref));
 	tools_swap(x_attr, ref.x_attr);
