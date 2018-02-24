4fbbf7ee464d61e09df3ffa1d8dd8e6abebbb9c0
Denis Corbin <dar.linux@free.fr>
2018-02-24 19:06:29 +0100

    moving the get_version() call from libdar.hpp to a separate file to be useable by libdar5 the API v5 backward adaptation

---
+++ src/libdar/get_version.cpp
@@ -0,0 +1,214 @@
+/*********************************************************************/
+// dar - disk archive - a backup/restoration program
+// Copyright (C) 2002-2052 Denis Corbin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// to contact the author : http://dar.linux.free.fr/email.html
+/*********************************************************************/
+
+#include "../my_config.h"
+
+extern "C"
+{
+#if HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
+#if HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#if LIBLZO2_AVAILABLE
+#if HAVE_LZO_LZO1X_H
+#include <lzo/lzo1x.h>
+#endif
+#endif
+
+#if HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#if HAVE_GCRYPT_H
+#ifndef GCRYPT_NO_DEPRECATED
+#define GCRYPT_NO_DEPRECATED
+#endif
+#include <gcrypt.h>
+#endif
+
+#if LIBCURL_AVAILABLE
+#ifdef HAVE_CURL_CURL_H
+#include <curl/curl.h>
+#endif
+#endif
+
+#if HAVE_TIME_H
+#include <time.h>
+#endif
+
+#if HAVE_GPGME_H
+#include <gpgme.h>
+#endif
+} // end extern "C"
+
+#include <string>
+#include <iostream>
+#include "get_version.hpp"
+#include "erreurs.hpp"
+#include "nls_swap.hpp"
+
+using namespace std;
+
+namespace libdar
+{
+    static void libdar_init(bool init_libgcrypt_if_not_done); // drives the "libdar_initialized" variable
+
+    static bool libdar_initialized = false; //< static variable modified once during the first get_version call
+#ifdef CRYPTO_AVAILABLE
+    static bool libdar_initialized_gcrypt = false; //< to record whether libdar did initialized libgcrypt
+#endif
+
+
+    void get_version(U_I & major, U_I & minor, bool init_libgcrypt)
+    {
+	NLS_SWAP_IN;
+        major = LIBDAR_COMPILE_TIME_MAJOR;
+        minor = LIBDAR_COMPILE_TIME_MINOR;
+	libdar_init(init_libgcrypt);
+	NLS_SWAP_OUT;
+    }
+
+    void get_version(U_I & major, U_I & medium, U_I & minor, bool init_libgcrypt)
+    {
+	NLS_SWAP_IN;
+
+        major = LIBDAR_COMPILE_TIME_MAJOR;
+	medium = LIBDAR_COMPILE_TIME_MEDIUM;
+        minor = LIBDAR_COMPILE_TIME_MINOR;
+	libdar_init(init_libgcrypt);
+	NLS_SWAP_OUT;
+    }
+
+    static void libdar_init(bool init_libgcrypt_if_not_done)
+    {
+	if(!libdar_initialized)
+	{
+		// locale for gettext
+
+	    if(string(DAR_LOCALEDIR) != string(""))
+		if(bindtextdomain(PACKAGE, DAR_LOCALEDIR) == nullptr)
+		    throw Erange("", "Cannot open the translated messages directory, native language support will not work");
+
+		// pseudo random generator
+
+	    srand(::time(nullptr)+getpid()+getppid());
+
+		// initializing LIBLZO2
+
+#if HAVE_LIBLZO2
+	    if(lzo_init() != LZO_E_OK)
+		throw Erange("libdar_init_thread_safe", gettext("Initialization problem for liblzo2 library"));
+#endif
+
+		// initializing libgcrypt
+
+#ifdef CRYPTO_AVAILABLE
+	    if(!gcry_control(GCRYCTL_INITIALIZATION_FINISHED_P))
+	    {
+		if(init_libgcrypt_if_not_done)
+		{
+		    gcry_error_t err;
+
+			// libgcypt built-in memory guard must be called before gcry_check_version
+
+		    err = gcry_control(GCRYCTL_ENABLE_M_GUARD);
+		    if(err != GPG_ERR_NO_ERROR)
+			throw Erange("libdar_init",tools_printf(gettext("Error while activating libgcrypt's memory guard: %s/%s"), gcry_strsource(err),gcry_strerror(err)));
+
+			// no multi-thread support activated for gcrypt
+			// this must be done from the application as stated
+			// by the libgcrypt documentation
+
+		    if(!gcry_check_version(MIN_VERSION_GCRYPT))
+			throw Erange("libdar_init_libgcrypt", tools_printf(gettext("Too old version for libgcrypt, minimum required version is %s\n"), MIN_VERSION_GCRYPT));
+
+			// initializing default sized secured memory for libgcrypt
+		    (void)gcry_control(GCRYCTL_INIT_SECMEM, 262144);
+			// if secured memory could not be allocated, further request of secured memory will fail
+			// and a warning will show at that time (we cannot send a warning (just failure notice) at that time).
+
+		    err = gcry_control(GCRYCTL_INITIALIZATION_FINISHED, 0);
+		    if(err != GPG_ERR_NO_ERROR)
+			throw Erange("libdar_init",tools_printf(gettext("Error while telling libgcrypt that initialization is finished: %s/%s"), gcry_strsource(err),gcry_strerror(err)));
+
+		    libdar_initialized_gcrypt = true;
+		}
+		else
+		    throw Erange("libdar_init_libgcrypt", gettext("libgcrypt not initialized and libdar not allowed to do so"));
+	    }
+	    else
+		if(!gcry_check_version(MIN_VERSION_GCRYPT))
+		    throw Erange("libdar_init_libgcrypt", tools_printf(gettext("Too old version for libgcrypt, minimum required version is %s\n"), MIN_VERSION_GCRYPT));
+#endif
+
+	    // initializing gpgme
+
+#if GPGME_SUPPORT
+	    if(gpgme_check_version(GPGME_MIN_VERSION) == nullptr)
+	    {
+		string tmp = "GPGME_SUPPORT";
+		throw Erange("libdar_init_gpgme", tools_printf(gettext("GPGME version requirement is not satisfied, requires version > %s"), tmp.c_str()));
+	    }
+
+	    if(gpgme_err_code(gpgme_engine_check_version(GPGME_PROTOCOL_OpenPGP)) != GPG_ERR_NO_ERROR)
+		throw Erange("libdar_init_gpgme", tools_printf(gettext("GPGME engine not available: %s"), gpgme_get_protocol_name(GPGME_PROTOCOL_OpenPGP)));
+#endif
+	    // initializing libcurl
+
+#if LIBCURL_AVAILABLE
+	    CURLcode curlret = curl_global_init(CURL_GLOBAL_ALL);
+	    if(curlret != 0)
+	    {
+		const char *msg = curl_easy_strerror(curlret);
+		throw Erange("libdar_init_libcurl", tools_printf(gettext("libcurl initialization failed: %s"), msg));
+	    }
+	    const curl_version_info_data *cvers = curl_version_info(CURLVERSION_FOURTH);
+	    if(cvers->age < CURLVERSION_FOURTH)
+		throw Erange(gettext("libcurl initialization failed: %s"), "libcurl version not available");
+	    if(cvers->version_num < 0x072600)
+		throw Erange(gettext("libcurl initialization failed: %s"), "libcurl version is too old");
+#endif
+
+	    tools_init();
+
+		// so now libdar is ready for use!
+
+	    libdar_initialized = true;
+	}
+    }
+
+    extern void close_and_clean()
+    {
+#ifdef CRYPTO_AVAILABLE
+	if(libdar_initialized_gcrypt)
+	    gcry_control(GCRYCTL_TERM_SECMEM, 0); // by precaution if not already done by libgcrypt itself
+#endif
+#if LIBCURL_AVAILABLE
+	curl_global_cleanup();
+#endif
+	tools_end();
+    }
+
+} // end of namespace
---
+++ src/libdar/get_version.hpp
@@ -0,0 +1,136 @@
+/*********************************************************************/
+// dar - disk archive - a backup/restoration program
+// Copyright (C) 2002-2052 Denis Corbin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// to contact the author : http://dar.linux.free.fr/email.html
+/*********************************************************************/
+
+    /// \file get_version.hpp
+    /// \brief routine to initialize libdar and manage its running threads
+    /// \ingroup API
+
+
+
+#ifndef GET_VERSION_HPP
+#define GET_VERSION_HPP
+
+#include "../my_config.h"
+
+extern "C"
+{
+#if MUTEX_WORKS
+#if HAVE_PTHREAD_H
+#include <pthread.h>
+#endif
+#endif
+}
+
+#include <string>
+#include "integers.hpp"
+#include "thread_cancellation.hpp"
+
+    /// libdar namespace encapsulate all libdar symbols
+
+namespace libdar
+{
+
+	/// \addtogroup API
+	/// @{
+
+	///  libdar Major version defined at compilation time
+    const U_I LIBDAR_COMPILE_TIME_MAJOR = 6;
+	///  libdar Medium version defined at compilation time
+    const U_I LIBDAR_COMPILE_TIME_MEDIUM = 0;
+	///  libdar Minor version defined at compilation time
+    const U_I LIBDAR_COMPILE_TIME_MINOR = 0;
+
+
+	////////////////////////////////////////////////////////////////////////
+	// LIBDAR INITIALIZATION METHODS                                      //
+	//                                                                    //
+	//      A FUNCTION OF THE get_version*() FAMILY *MUST* BE CALLED      //
+	//            BEFORE ANY OTHER FUNCTION OF THIS LIBRARY               //
+	//                                                                    //
+	// CLIENT PROGRAM MUST CHECK THAT THE MAJOR NUMBER RETURNED           //
+	// BY THIS CALL IS NOT GREATER THAN THE VERSION USED AT COMPILATION   //
+        // TIME. IF SO, THE PROGRAM MUST ABORT AND RETURN A WARNING TO THE    //
+	// USER TELLING THE DYNAMICALLY LINKED VERSION IS TOO RECENT AND NOT  //
+	// COMPATIBLE WITH THIS SOFTWARE. THE MESSAGE MUST INVITE THE USER    //
+	// TO UPGRADE HIS SOFTWARE WITH A MORE RECENT VERSION COMPATIBLE WITH //
+	// THIS LIBDAR RELEASE.                                               //
+	////////////////////////////////////////////////////////////////////////
+
+	/// return the libdar version, and make libdar initialization (may throw Exceptions)
+
+	/// It is mandatory to call this function (or another one of the get_version* family)
+	/// \param[out] major the major number of the version
+	/// \param[out] medium the medium number of the version
+	/// \param[out] minor the minor number of the version
+	/// \param[in] init_libgcrypt whether to initialize libgcrypt if not already done (not used if libcrypt is not linked with libdar)
+	/// \note the calling application must match that the major function
+	/// is the same as the libdar used at compilation time. See API tutorial for a
+	/// sample code.
+    extern void get_version(U_I & major, U_I & medium, U_I & minor, bool init_libgcrypt = true);
+
+	///////////////////////////////////////////////
+	// CLOSING/CLEANING LIBDAR                   //
+	///////////////////////////////////////////////
+
+	// while libdar has only a single boolean as global variable
+	// that defines whether the library is initialized or not
+	// it must proceed to mutex, and dependent libraries initializations
+	// (liblzo, libgcrypt, etc.), which is done during the get_version() call
+	// Some library also need to clear some data so the following call
+	// is provided in that aim and must be called when libdar will no more
+	// be used by the application.
+
+    extern void close_and_clean();
+
+	///////////////////////////////////////////////
+	// THREAD CANCELLATION ROUTINES              //
+	///////////////////////////////////////////////
+
+#if MUTEX_WORKS
+	/// thread cancellation activation
+
+	/// ask that any libdar code running in the thread given as argument be cleanly aborted
+	/// when the execution will reach the next libdar checkpoint
+	/// \param[in] tid is the Thread ID to cancel libdar in
+	/// \param[in] immediate whether to cancel thread immediately or just signal the request to the thread
+	/// \param[in] flag an arbitrary value passed as-is through libdar
+    inline void cancel_thread(pthread_t tid, bool immediate = true, U_64 flag = 0) { thread_cancellation::cancel(tid, immediate, flag); }
+
+	/// consultation of the cancellation status of a given thread
+
+	/// \param[in] tid is the tid of the thread to get status about
+	/// \return false if no cancellation has been requested for the given thread
+    inline bool cancel_status(pthread_t tid) { return thread_cancellation::cancel_status(tid); }
+
+	/// thread cancellation deactivation
+
+	/// abort the thread cancellation for the given thread
+	/// \return false if no thread cancellation was under process for that thread
+	/// or if there is no more pending cancellation (thread has already been canceled).
+    inline bool cancel_clear(pthread_t tid) { return thread_cancellation::clear_pending_request(tid); }
+#endif
+
+
+	/// @}
+
+} // end of namespace
+
+#endif
