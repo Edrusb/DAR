53ff0c467bbaabde267910157e62a3d34b286e2d
Denis Corbin <dar.linux@free.fr>
2018-02-24 19:11:13 +0100

    updating Changelog

--- src/libdar/libdar5.hpp
+++ src/libdar/libdar5.hpp
@@ -396,7 +396,7 @@ namespace libdar5
 	///  libdar Medium version defined at compilation time
     const U_I LIBDAR_COMPILE_TIME_MEDIUM = 100;
 	///  libdar Minor version defined at compilation time
-    const U_I LIBDAR_COMPILE_TIME_MINOR = 1;
+    const U_I LIBDAR_COMPILE_TIME_MINOR = 0;
 
 	////////////////////////////////////////////////////////////////////////
 	// LIBDAR INITIALIZATION METHODS                                      //
