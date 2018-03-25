90fb7134c3be1fa18dd51fc05c518ea8eae7f71c
Denis Corbin <dar.linux@free.fr>
2018-03-25 21:51:55 +0200

    fixing missing argument in doxygen documentation for archive::get_children_in_table() method

--- src/libdar/archive.hpp
+++ src/libdar/archive.hpp
@@ -283,6 +283,7 @@ namespace libdar
 	    /// getting information about the given directory (alternative to get_children_of)
 	    ///
 	    /// \param[in] dir relative path the directory to get information about, use empty string for root directory
+	    /// \param[in] fetch_ea whether to fetch Extended Attributes relative information in each returned list_entry
 	    /// \return a table information about all subdir and subfile for the given directory
 	    /// \note at the difference of get_children_of, this call does not rely on a user_interaction class
 	    /// to provide the information, but rather returns a table of children. To allow new fields to
