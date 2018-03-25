bed11121c2037c578d1b14371dfbdeee7201709d
Denis Corbin <dar.linux@free.fr>
2018-03-25 20:53:14 +0200

    adding new fields and methods to list_entry
    added or modified methods and the following and their set_* conterpart
    list_entry::get_removed_type()
    list_entry::get_removed_type_description()
    list_entry::get_data_flag()
    list_entry::get_ea_flag()
    list_entry::get_fsa_flag()
    list_entry::get_uid(bool try_resolving_name)
    list_entry::get_gid(bool try_resolving_name)
    list_entry::get_removal_date()
    list_entry::get_removal_date_s()
    list_entry::get_file_size(bool size_in_bytes)
    list_entry::get_compression_ratio_flag()
    list_entry::get_delta_flag()
    list_entry::get_ea_read_next(std::string & key)

--- src/libdar/list_entry.cpp
+++ src/libdar/list_entry.cpp
@@ -27,26 +27,233 @@ using namespace std;
 
 namespace libdar
 {
-    list_entry::list_entry()
+
+    unsigned char list_entry::get_removed_type() const
     {
-	my_name = "";
-	type = ' ';
-	hard_link = false;
-	uid = 0;
-	gid = 0;
-	perm = 0;
-	data_status = saved_status::saved;
-	ea_status = ea_saved_status::none;
-	fsa_status = fsa_saved_status::none;
-	file_size = 0;
-	storage_size = 0;
-	sparse_file = 0;
-	compression_algo = compression::none;
-	dirty = false;
-	target = "";
-	major = 0;
-	minor = 0;
-	delta_sig = false;
+	if(is_removed_entry())
+	{
+	    if(target.size() != 1)
+		throw SRC_BUG;
+	    return target[0];
+	}
+	else
+	    return '!';
+    }
+
+    string list_entry::get_removed_type_description() const
+    {
+	if(is_removed_entry())
+	{
+	    if(target.size() != 1)
+		return type_to_string(target[0]);
+	    else
+		return gettext("unspecified type");
+	}
+	else
+	    return "";
+    }
+
+    string list_entry::get_data_flag() const
+    {
+	switch(data_status)
+	{
+	case saved_status::saved:
+	    if(dirty)
+		return gettext("[DIRTY]");
+	    else
+		return gettext("[Saved]");
+	case saved_status::inode_only:
+	    return gettext("[Inode]");
+	case saved_status::fake:
+	    return gettext("[InRef]");
+	case saved_status::not_saved:
+	    return "[     ]";
+	case saved_status::delta:
+	    return "[Delta]";
+	default:
+	    throw SRC_BUG;
+	}
+    }
+
+    string list_entry::get_ea_flag() const
+    {
+	switch(ea_status)
+	{
+	case ea_saved_status::full:
+	    return gettext("[Saved]");
+	case ea_saved_status::fake:
+	    return gettext("[InRef]");
+	case ea_saved_status::partial:
+	    return "[     ]";
+	case ea_saved_status::none:
+	    return "       ";
+	case ea_saved_status::removed:
+	    return "[Suppr]";
+	default:
+	    throw SRC_BUG;
+	}
+    }
+
+    string list_entry::get_fsa_flag() const
+    {
+	string ret = "";
+
+	if(fsa_status != fsa_saved_status::none)
+	{
+	    fsa_scope sc = get_fsa_scope();
+	    bool upper = fsa_status == fsa_saved_status::full;
+	    ret = fsa_scope_to_string(upper, sc);
+	    while(ret.size() < 3)
+		ret += "-";
+	    ret = "[" + ret + "]";
+	}
+	else
+	    ret = "[---]";
+
+	return ret;
+    }
+
+    string list_entry::get_uid(bool try_resolving_name) const
+    {
+	if(try_resolving_name)
+	    return tools_name_of_uid(uid);
+	else
+	    return deci(uid).human();
+    }
+
+    string list_entry::get_gid(bool try_resolving_name) const
+    {
+	if(try_resolving_name)
+	    return tools_name_of_gid(gid);
+	else
+	    return deci(gid).human();
+    }
+
+    string list_entry::get_last_modif() const
+    {
+	if(!is_removed_entry())
+	    return last_modif.is_null() ? "" : tools_display_date(last_modif);
+	else
+	    return "";
+    }
+
+    string list_entry::get_removal_date() const
+    {
+	if(is_removed_entry())
+	    return last_modif.is_null() ? "Unknown date" : tools_display_date(last_modif);
+	else
+	    return "";
+    }
+
+    time_t list_entry::get_last_modif_s() const
+    {
+	if(!is_removed_entry())
+	    return datetime2time_t(last_modif);
+	else
+	    return 0;
+    }
+
+    time_t list_entry::get_removal_date_s() const
+    {
+	if(is_removed_entry())
+	    return datetime2time_t(last_modif);
+	else
+	    return 0;
+    }
+
+    string list_entry::get_file_size(bool size_in_bytes) const
+    {
+	if(size_in_bytes)
+	    return deci(file_size).human();
+	else
+	    return tools_display_integer_in_metric_system(file_size , "o", true);
+    }
+
+    string list_entry::get_compression_ratio() const
+    {
+	if((is_file() && has_data_present_in_the_archive())
+	   || is_dir())
+	    return tools_get_compression_ratio(storage_size,
+					       file_size,
+					       compression_algo != compression::none
+					       || is_dir());
+	else
+	    return "";
+    }
+
+    string list_entry::get_compression_ratio_flag() const
+    {
+	string ret = get_compression_ratio();
+
+	if(ret.size() == 0)
+	    return "[-----]";
+	else
+	    return "[" + ret + "]";
+    }
+
+    string list_entry::get_delta_flag() const
+    {
+	if(!is_file())
+	    return "[-]";
+	else
+	{
+	    if(has_delta_signature())
+		return "[D]";
+	    else
+		return "[ ]";
+	}
+    }
+
+    string list_entry::get_storage_size_for_data(bool size_in_bytes) const
+    {
+	if(size_in_bytes)
+	    return deci(storage_size_for_data).human();
+	else
+	    return tools_display_integer_in_metric_system(storage_size_for_data, "o", true);
+    }
+
+    bool list_entry::get_ea_read_next(std::string & key) const
+    {
+	if(it_ea != ea.end())
+	{
+	    key = *it_ea;
+	    ++it_ea;
+	    return true;
+	}
+	else
+	    return false;
+    }
+
+    void list_entry::set_removal_date(const datetime & val)
+    {
+	if(!is_removed_entry())
+	    throw SRC_BUG;
+	last_modif = val;
+	    // we recycle last_modif for this purpose
+	    // when the entry is a "removed" one
+    }
+
+    void list_entry::set_removed_type(unsigned char val)
+    {
+	if(!is_removed_entry())
+	    throw SRC_BUG;
+
+	target.clear();
+	target += val;
+	if(target.size() != 1)
+	    throw SRC_BUG;
+    }
+
+    void list_entry::set_ea(const ea_attributs & arg)
+    {
+	string key, val;
+
+	ea.clear();
+
+	arg.reset_read();
+	while(arg.read(key, val))
+	    ea.push_back(key); // we ignore val
+	get_ea_reset_read();
     }
 
     void list_entry::clear()
@@ -79,6 +286,9 @@ namespace libdar
 	storage_size_for_EA = 0;
 	offset_for_FSA = 0;
 	storage_size_for_FSA = 0;
+	ea.clear();
+	it_ea = ea.begin();
+	etiquette = 0;
     }
 
     time_t list_entry::datetime2time_t(const datetime & val)
@@ -89,6 +299,41 @@ namespace libdar
 	(void) val.get_value(ret, unused, datetime::tu_second);
 
 	return ret;
+    }
+
+    string list_entry::type_to_string(unsigned char type)
+    {
+	switch(type)
+	{
+	case 'j':
+	    return gettext("ignored directory");
+	case 'd':
+	    return gettext("folder");
+	case 'x':
+	    return gettext("deleted file");
+	case 'o':
+	    return gettext("door");
+	case 'f':
+	    return gettext("file");
+	case 'l':
+	    return gettext("symlink");
+	case 'c':
+	    return gettext("char device");
+	case 'b':
+	    return gettext("block device");
+	case 'p':
+	    return gettext("pipe");
+	case 's':
+	    return gettext("socket");
+	case 'i':
+	    return gettext("ignored entry");
+	case 'm':
+	    return gettext("hard linked inode");
+	case 'z':
+	    return gettext("end of directory");
+	default:
+	    throw SRC_BUG; // missing inode type
+	}
     }
 
 } // end of namespace
--- src/libdar/list_entry.hpp
+++ src/libdar/list_entry.hpp
@@ -39,6 +39,8 @@
 #include "datetime.hpp"
 #include "range.hpp"
 #include "cat_status.hpp"
+#include "ea.hpp"
+#include "fsa_family.hpp"
 
 namespace libdar
 {
@@ -54,7 +56,7 @@ namespace libdar
     class list_entry
     {
     public:
-	list_entry();
+	list_entry() { clear(); };
 	list_entry(const list_entry & ref) = default;
 	list_entry(list_entry && ref) noexcept = default;
 	list_entry & operator = (const list_entry & ref) = default;
@@ -73,6 +75,7 @@ namespace libdar
 
 	const std::string & get_name() const { return my_name; };
 	unsigned char get_type() const { return type; };
+	std::string get_type_description() const { return type_to_string(type); };
 	bool is_dir() const { return type == 'd'; };
 	bool is_file() const { return type == 'f'; };
 	bool is_symlink() const { return type == 'l'; };
@@ -84,21 +87,33 @@ namespace libdar
 	bool is_removed_entry() const { return type == 'x'; };
 	bool is_door_inode() const { return type == 'o'; };
 
-	bool has_data_present_in_the_archive() const { return data_status == saved_status::saved; };
+	unsigned char get_removed_type() const;  ///< valid only for removed_entries
+	std::string get_removed_type_description() const; ///< valid only for removed_entries
+
+	bool has_data_present_in_the_archive() const { return data_status == saved_status::saved || data_status == saved_status::delta; };
+	std::string get_data_flag() const;
+	saved_status get_data_status() const { return data_status; };
+
 	bool has_EA() const { return ea_status != ea_saved_status::none && ea_status != ea_saved_status::removed; };
 	bool has_EA_saved_in_the_archive() const { return ea_status == ea_saved_status::full; };
+	std::string get_ea_flag() const;
+	ea_saved_status get_ea_status() const { return ea_status; };
+
 	bool has_FSA() const { return fsa_status != fsa_saved_status::none; };
 	bool has_FSA_saved_in_the_archive() const { return fsa_status == fsa_saved_status::full; };
+	std::string get_fsa_flag() const;
 
-	std::string get_uid() const { return deci(uid).human(); };
-	std::string get_gid() const { return deci(gid).human(); };
+	std::string get_uid(bool try_resolving_name = false) const;
+	std::string get_gid(bool try_resolving_name = false) const;
 	std::string get_perm() const { return tools_get_permission_string(type, perm, hard_link); };
 	std::string get_last_access() const { return last_access.is_null() ? "" : tools_display_date(last_access); };
-	std::string get_last_modif() const { return last_modif.is_null() ? "" : tools_display_date(last_modif); };
+	std::string get_last_modif() const;
 	std::string get_last_change() const { return last_change.is_null() ? "" : tools_display_date(last_change); };
+	std::string get_removal_date() const; ///< for removed_entry only
 	time_t get_last_access_s() const { return datetime2time_t(last_access); };
-	time_t get_last_modif_s() const { return datetime2time_t(last_modif); };
+	time_t get_last_modif_s() const;
 	time_t get_last_change_s() const { return datetime2time_t(last_change); };
+	time_t get_removal_date_s() const; ///< for removed_entry only
 
 	    /// yet an alternative method to get last access time
 	    ///
@@ -116,9 +131,11 @@ namespace libdar
 	void get_last_change(datetime::time_unit tu, time_t & second, time_t & fraction) const
 	{ last_change.get_value(second, fraction, tu); }
 
-	std::string get_file_size() const { return deci(file_size).human(); };
-	std::string get_compression_ratio() const { return tools_get_compression_ratio(storage_size, file_size, compression_algo != compression::none); };
+	std::string get_file_size(bool size_in_bytes = true) const;
+	std::string get_compression_ratio() const;
+	std::string get_compression_ratio_flag() const;
 	bool is_sparse() const { return sparse_file; };
+	std::string get_sparse_flag() const { return sparse_file ? "[X]" : "[ ]"; };
 	std::string get_compression_algo() const { return compression2string(compression_algo); };
 	bool is_dirty() const { return dirty; };
 	std::string get_link_target() const { return target; };
@@ -126,6 +143,7 @@ namespace libdar
 	std::string get_minor() const { return tools_int2str(minor); };
 	const range & get_slices() const { return slices; };
 	bool has_delta_signature() const { return delta_sig; };
+	std::string get_delta_flag() const;
 
 
 	    /// offset in byte where to find first byte of data
@@ -149,7 +167,7 @@ namespace libdar
 	    /// infinint of std::string version of this method
 	bool get_storage_size_for_data(infinint & val) const { val = storage_size_for_data; return !val.is_zero(); };
 	bool get_storage_size_for_data(U_64 & val) const { return tools_infinint2U_64(storage_size_for_data, val) && !storage_size_for_data.is_zero(); };
-	std::string get_storage_size_for_data() const { return storage_size_for_data.is_zero() ? "" : deci(storage_size_for_data).human(); };
+	std::string get_storage_size_for_data(bool size_in_bytes = true) const;
 
 	    /// offset in byte whert to find the first byte of Extended Attributes
 	    ///
@@ -179,17 +197,29 @@ namespace libdar
 	bool get_storage_size_for_FSA(U_64 & val) const { return tools_infinint2U_64(storage_size_for_FSA, val) && !storage_size_for_FSA.is_zero(); };
 	std::string get_storage_size_for_FSA() const { return storage_size_for_FSA.is_zero() ? "" : deci(storage_size_for_FSA).human(); };
 
+	void get_ea_reset_read() const { it_ea = ea.begin(); };
+	bool get_ea_read_next(std::string & key) const;
+
+	std::string get_etiquette() const { return deci(etiquette).human(); }; ///< this is the hard-link ID, only valid for hard linked entries
+
+	fsa_scope get_fsa_scope() const { return fsa_sc; };
+
+	std::string get_data_crc() const { return data_crc; };
+	std::string get_delta_patch_base_crc() const { return patch_base_crc; };
+	std::string get_delta_patch_result_crc() const { return patch_result_crc; };
 
 	    // methods for libdar to setup the object
 
 	void set_name(const std::string & val) { my_name = val; };
 	void set_type(unsigned char val) { type = val; };
+	void set_removed_type(unsigned char val);
 	void set_hard_link(bool val) { hard_link = val; };
 	void set_uid(const infinint & val) { uid = val; };
 	void set_gid(const infinint & val) { gid = val; };
 	void set_perm(U_16 val) { perm = val; };
 	void set_last_access(const datetime & val) { last_access = val; };
 	void set_last_modif(const datetime & val) { last_modif = val; };
+	void set_removal_date(const datetime & val);
 	void set_saved_status(saved_status val) { data_status = val; };
 	void set_ea_status(ea_saved_status val) { ea_status = val; };
 	void set_last_change(const datetime & val) { last_change = val; };
@@ -210,6 +240,12 @@ namespace libdar
 	void set_storage_size_for_EA(const infinint & val) { storage_size_for_EA = val; };
 	void set_archive_offset_for_FSA(const infinint & val) { offset_for_FSA = val; };
 	void set_storage_size_for_FSA(const infinint & val) { storage_size_for_FSA = val; };
+	void set_ea(const ea_attributs & arg);
+	void set_etiquette(const infinint & arg) { etiquette = arg; };
+	void set_fsa_scope(const fsa_scope & arg) { fsa_sc = arg; };
+	void set_data_crc(const crc & ptr) { data_crc = ptr.crc2str(); };
+	void set_delta_patch_base_crc(const crc & ptr) { patch_base_crc = ptr.crc2str(); };
+	void set_delta_patch_result_crc(const crc & ptr) { patch_result_crc = ptr.crc2str(); };
 
 	void clear();
 
@@ -221,17 +257,18 @@ namespace libdar
 	infinint gid;
 	U_16 perm;
 	datetime last_access;
-	datetime last_modif;
+	datetime last_modif;     ///< also used to store removal_date for removed entries
 	saved_status data_status;
 	ea_saved_status ea_status;
 	datetime last_change;
 	fsa_saved_status fsa_status;
+	fsa_scope fsa_sc;
 	infinint file_size;
 	infinint storage_size;
 	bool sparse_file;
 	compression compression_algo;
 	bool dirty;
-	std::string target;
+	std::string target;    ///< target[0] used to store signature of removed entry for removed entries
 	int major;
 	int minor;
 	range slices;
@@ -242,8 +279,15 @@ namespace libdar
 	infinint storage_size_for_EA;
 	infinint offset_for_FSA;
 	infinint storage_size_for_FSA;
+	std::deque<std::string> ea;
+	mutable std::deque<std::string>::const_iterator it_ea;
+	infinint etiquette;  ///< hard link identification
+	std::string data_crc;
+	std::string patch_base_crc;
+	std::string patch_result_crc;
 
 	static time_t datetime2time_t(const datetime & val);
+	static std::string type_to_string(unsigned char type);
     };
 
 } // end of namespace
