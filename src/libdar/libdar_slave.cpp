b765ad5318ac5e7a1c560ef525c894484c7bf799
Denis Corbin <dar.linux@free.fr>
2018-04-13 19:43:40 +0200

    adding dar_slave implementation in libdar as class libdar_slave

---
+++ src/libdar/libdar_slave.cpp
@@ -0,0 +1,96 @@
+/*********************************************************************/
+// dar - disk archive - a backup/restoration program
+// Copyright (C) 2002-2052 Denis Corbin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// to contact the author : http://dar.linux.free.fr/email.html
+/*********************************************************************/
+
+#include "../my_config.h"
+
+extern "C"
+{
+} // end extern "C"
+
+#include <string>
+#include <new>
+
+#include "libdar_slave.hpp"
+#include "path.hpp"
+
+using namespace std;
+
+namespace libdar
+{
+
+    libdar_slave::libdar_slave(shared_ptr<user_interaction> & dialog,
+			       const string & folder,
+			       const string & basename,
+			       const string & extension,
+			       const string & input_pipe,
+			       const string & output_pipe,
+			       const string & execute,
+			       const infinint & min_digits)
+    {
+	path chemin(folder);
+	tuyau *input = nullptr;
+	tuyau *output = nullptr;
+	sar *source = nullptr;
+
+	entrep.reset(new (nothrow) entrepot_local("", "", false));
+	if(!entrep)
+	    throw Ememory("libdar_slave::libdar_slave");
+
+	entrep->set_location(chemin);
+
+	try
+	{
+	    source = new (nothrow) sar(dialog,
+				       basename,
+				       extension,
+				       *entrep,
+				       true,
+				       min_digits,
+				       false,
+				       execute);
+	    if(source == nullptr)
+		throw Ememory("libdar_slave::libdar_slave");
+
+	    tools_open_pipes(dialog,
+			     input_pipe,
+			     output_pipe,
+			     input,
+			     output);
+
+	    zap.reset(new (nothrow) slave_zapette(input, output, source));
+	    if(!zap)
+		throw Ememory("libdar_slave::libdar_slave");
+            input = output = nullptr; // now managed by zap;
+            source = nullptr;  // now managed by zap;
+	}
+	catch(...)
+	{
+	    if(input != nullptr)
+		delete input;
+	    if(output != nullptr)
+		delete output;
+	    if(source != nullptr)
+		delete source;
+	    throw;
+	}
+    }
+
+} // end of namespace
---
+++ src/libdar/libdar_slave.hpp
@@ -0,0 +1,70 @@
+/*********************************************************************/
+// dar - disk archive - a backup/restoration program
+// Copyright (C) 2002-2052 Denis Corbin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// to contact the author : http://dar.linux.free.fr/email.html
+/*********************************************************************/
+
+    /// \file libdar_slave.hpp
+    /// \brief API for dar_slave functionnality
+    /// \ingroup API
+
+#ifndef LIBDAR_SLAVE_HPP
+#define LIBDAR_SLAVE_HPP
+
+#include "../my_config.h"
+#include "tuyau.hpp"
+#include "entrepot_local.hpp"
+#include "sar.hpp"
+#include "slave_zapette.hpp"
+#include "infinint.hpp"
+
+namespace libdar
+{
+
+	/// \addtogroup API
+	/// @{
+
+    class libdar_slave
+    {
+    public:
+	libdar_slave(std::shared_ptr<user_interaction> & dialog,
+		     const std::string & folder,
+		     const std::string & basename,
+		     const std::string & extension,
+		     const std::string & input_pipe,
+		     const std::string & output_pipe,
+		     const std::string & execute,
+		     const infinint & min_digits);
+	libdar_slave(const libdar_slave & ref) = delete;
+	libdar_slave(libdar_slave && ref) noexcept = default;
+	libdar_slave & operator = (const libdar_slave & ref) = delete;
+	libdar_slave & operator = (libdar_slave && ref) noexcept = default;
+	~libdar_slave() { zap.reset(); entrep.reset(); }; // the order is important
+
+	void run() { zap->action(); };
+
+    private:
+	std::unique_ptr<entrepot_local> entrep; // must be deleted last
+	std::unique_ptr<slave_zapette> zap; // must be deleted first
+    };
+
+	/// @}
+
+} // end of namespace
+
+#endif
