6f1ee0b60bee652eff5d9f77fdd928d0160cb897
Denis Corbin <dar.linux@free.fr>
2018-07-09 18:39:11 +0200

    activate needed linux capability in the effective set if it is permitted but not effective

--- src/libdar/capabilities.cpp
+++ src/libdar/capabilities.cpp
@@ -61,17 +61,41 @@ namespace libdar
 
 #else
 
-    static capa_status cap_check(cap_value_t capa, user_interaction & ui, bool verbose, const std::string & capa_name);
+    static capa_status lxcapa_check(cap_value_t capa,
+				    cap_flag_t capa_from_set,
+				    user_interaction & ui,
+				    bool verbose,
+				    const std::string & capa_name);
 
-    capa_status capability_LINUX_IMMUTABLE(user_interaction & ui, bool verbose) { return cap_check(CAP_LINUX_IMMUTABLE, ui, verbose, "Immutable"); }
+    static bool lxcapa_set(cap_value_t capa,
+			   cap_flag_t capa_from_set,
+			   bool value,
+			   user_interaction & ui,
+			   bool verbose,
+			   const std::string & capa_name);
 
-    capa_status capability_SYS_RESOURCE(user_interaction & ui, bool verbose) { return cap_check(CAP_SYS_RESOURCE, ui, verbose, "System Resource"); }
+    capa_status lxcapa_activate(cap_value_t capa,
+				user_interaction & ui,
+				bool verbose,
+				const std::string & capa_name);
 
-    capa_status capability_FOWNER(user_interaction & ui, bool verbose) { return cap_check(CAP_FOWNER, ui, verbose, "File Owner for all files"); }
+    capa_status capability_LINUX_IMMUTABLE(user_interaction & ui, bool verbose)
+    { return lxcapa_activate(CAP_LINUX_IMMUTABLE, ui, verbose, "Immutable"); }
 
-    capa_status capability_CHOWN(user_interaction & ui, bool verbose) { return cap_check(CAP_CHOWN, ui, verbose, "change ownership"); }
+    capa_status capability_SYS_RESOURCE(user_interaction & ui, bool verbose)
+    { return lxcapa_activate(CAP_SYS_RESOURCE, ui, verbose, "System Resource"); }
 
-    static capa_status cap_check(cap_value_t capa, user_interaction & ui, bool verbose, const std::string & capa_name)
+    capa_status capability_FOWNER(user_interaction & ui, bool verbose)
+    { return lxcapa_activate(CAP_FOWNER, ui, verbose, "File Owner for all files"); }
+
+    capa_status capability_CHOWN(user_interaction & ui, bool verbose)
+    { return lxcapa_activate(CAP_CHOWN, ui, verbose, "change ownership"); }
+
+    static capa_status lxcapa_check(cap_value_t capa,
+				    cap_flag_t capa_from_set,
+				    user_interaction & ui,
+				    bool verbose,
+				    const std::string & capa_name)
     {
 	capa_status ret = capa_unknown;
 	cap_t capaset = cap_get_proc();
@@ -79,7 +103,7 @@ namespace libdar
 
 	try
 	{
-	    if(cap_get_flag(capaset, capa, CAP_EFFECTIVE, &val) == 0)
+	    if(cap_get_flag(capaset, capa, capa_from_set, &val) == 0)
 		ret = (val == CAP_SET) ? capa_set : capa_clear;
 	    else
 	    {
@@ -100,6 +124,89 @@ namespace libdar
 
 	return ret;
     }
+
+    static bool lxcapa_set(cap_value_t capa,
+			   cap_flag_t capa_from_set,
+			   bool value,
+			   user_interaction & ui,
+			   bool verbose,
+			   const std::string & capa_name)
+    {
+	cap_t capaset = cap_get_proc();
+	cap_flag_value_t flag_value = value ? CAP_SET : CAP_CLEAR;
+	bool ret = false;
+
+	try
+	{
+	    if(cap_set_flag(capaset, capa_from_set, 1, &capa, flag_value) != 0)
+	    {
+		string tmp = tools_strerror_r(errno);
+		ui.printf(gettext("Error met while setting capability %S: %s"), &capa_name, tmp.c_str());
+	    }
+	    else // no error so far
+	    {
+		if(cap_set_proc(capaset) != 0)
+		{
+		    string tmp = tools_strerror_r(errno);
+		    ui.printf(gettext("Error met while setting capability %S: %s"), &capa_name, tmp.c_str());
+		}
+		else // no error so far
+		    ret = true;
+	    }
+	}
+	catch(...) // well a try/catch may seems useless here, but it does not hurt ... :-)
+	{
+	    cap_free(capaset);
+	    throw;
+	}
+	cap_free(capaset);
+
+	return ret;
+    }
+
+    capa_status lxcapa_activate(cap_value_t capa,
+				user_interaction & ui,
+				bool verbose,
+				const std::string & capa_name)
+    {
+	capa_status ret = lxcapa_check(capa,
+				       CAP_EFFECTIVE,
+				       ui,
+				       verbose,
+				       capa_name);
+
+	if(ret == capa_clear)
+	{
+		// maybe capability is available in CAP_PERMITTED
+
+	    if(lxcapa_check(capa,
+			    CAP_PERMITTED,
+			    ui,
+			    verbose,
+			    capa_name) == capa_set)
+	    {
+		    // yes it is we can try activating the capability in CAP_EFFECTIVE
+		if(lxcapa_set(capa,
+			      CAP_EFFECTIVE,
+			      true,
+			      ui,
+			      verbose,
+			      capa_name))
+		{
+			// checking if the status has changed
+		    ret = lxcapa_check(capa,
+				       CAP_EFFECTIVE,
+				       ui,
+				       verbose,
+				       capa_name);
+		}
+	    }
+		// capability not in CAP_PERMITTED, not trying to activating it
+	}
+
+	return ret;
+    }
+
 
 #endif
 
