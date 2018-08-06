4a156874a600a07284adb15bec15455302426ef8
Denis Corbin <dar.linux@free.fr>
2018-08-06 21:17:32 +0200

    adding copyright header to misc/code

--- misc/todos.c
+++ misc/todos.c
@@ -1,3 +1,24 @@
+/*********************************************************************
+// dar - disk archive - a backup/restoration program
+// Copyright (C) 2002-2052 Denis Corbin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// to contact the author : http://dar.linux.free.fr/email.html
+*********************************************************************/
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -16,7 +37,7 @@ int main(int argc,char *argv[])
     char *file;
     int p;
     register int i,lu,d;
-   
+
     if(argc < 2)
     {
         printf("usage : %s liste de fichiers\n",argv[0]);
