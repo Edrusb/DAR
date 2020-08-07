0dcddd38d09e563d27c13de161d10179093e6530
Denis Corbin <dar.linux@free.fr>
2020-08-07 11:18:34 +0200

    adding the class proto_tronco, common parent of tronconneuse and parallel_tronconneuse classes

---
+++ src/libdar/proto_tronco.hpp
@@ -0,0 +1,64 @@
+/*********************************************************************/
+// dar - disk archive - a backup/restoration program
+// Copyright (C) 2002-2020 Denis Corbin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// to contact the author : http://dar.linux.free.fr/email.html
+/*********************************************************************/
+
+    /// \file proto_tronco.hpp
+    /// \brief defines common interface for tronconneuse and parallel_tronconneuse
+    /// \ingroup Private
+    ///
+
+#ifndef PROTO_TRONCO_HPP
+#define PROTO_TRONCO_HPP
+
+#include "../my_config.h"
+#include <string>
+
+#include "infinint.hpp"
+#include "integers.hpp"
+#include "generic_file.hpp"
+#include "archive_version.hpp"
+#include "generic_file.hpp"
+
+namespace libdar
+{
+
+	/// \addtogroup Private
+	/// @{
+
+
+    typedef infinint (*trailing_clear_data_callback)(generic_file & below, const archive_version & reading_ver);
+
+    class proto_tronco: public generic_file
+    {
+    public:
+	using generic_file::generic_file;
+
+	virtual void set_initial_shift(const infinint & x) = 0;
+	virtual void set_callback_trailing_clear_data(trailing_clear_data_callback) = 0;
+	virtual U_32 get_clear_block_size() const = 0;
+	virtual	void write_end_of_file() = 0;
+
+    };
+
+	/// @}
+
+} // end of namespace
+
+#endif
