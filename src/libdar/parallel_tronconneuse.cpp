215f0ab6cb2145e1f67fe526eec1a2956a9f6eb5
Denis Corbin <dar.linux@free.fr>
2020-08-17 22:16:59 +0200

    fixing bug in parallel_tronconneuse, not the same mode of startup of subthreads in read-mode than in write-mode

--- src/libdar/parallel_tronconneuse.cpp
+++ src/libdar/parallel_tronconneuse.cpp
@@ -457,6 +457,9 @@ namespace libdar
 		    throw Ememory("parallel_tronconneuse::parallel_tronconneuse");
 		else
 		    crypto_reader->run();
+
+		    // in read mode the threads are released when
+		    // necessary by the go_read() subcall
 		break;
 	    case gf_write_only:
 		crypto_writer = make_unique<write_below>(gather,
@@ -468,6 +471,11 @@ namespace libdar
 		    throw Ememory("parallel_tronconneuse::parallel_tronconneuse");
 		else
 		    crypto_writer->run();
+
+		    // all subthreads are pending on waiter barrier
+
+		waiter->wait(); // release all threads here in write mode
+
 		break;
 	    case gf_read_write:
 		throw SRC_BUG;
@@ -475,9 +483,7 @@ namespace libdar
 		throw SRC_BUG;
 	    }
 
-		// all subthreads are pending on waiter barrier
 
-	    waiter->wait(); // release all threads
 
 	}
 	catch(std::bad_alloc &)
