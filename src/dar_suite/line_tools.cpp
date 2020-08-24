84b20dd3fb232f4af229b18eb4df1447e945e12f
Denis Corbin <dar.linux@free.fr>
2020-08-24 19:23:00 +0200

    adding liblz4 information in the version summary provided by dar -V

--- src/dar_suite/line_tools.cpp
+++ src/dar_suite/line_tools.cpp
@@ -1347,6 +1347,7 @@ void line_tools_display_features(user_interaction & dialog)
 	dialog.printf(gettext("   Liblzo2 compression (lzo)    : %s"), YES_NO(compile_time::liblzo()));
 	dialog.printf(gettext("   Liblzma compression (xz)     : %s"), YES_NO(compile_time::libxz()));
 	dialog.printf(gettext("   Liblzstd compression (zstd)  : %s"), YES_NO(compile_time::libzstd()));
+	dialog.printf(gettext("   Liblzstd compression (lz4)   : %s"), YES_NO(compile_time::liblz4()));
 	dialog.printf(gettext("   Strong encryption (libgcrypt): %s"), YES_NO(compile_time::libgcrypt()));
 	dialog.printf(gettext("   Public key ciphers (gpgme)   : %s"), YES_NO(compile_time::public_key_cipher()));
 	dialog.printf(gettext("   Extended Attributes support  : %s"), YES_NO(compile_time::ea()));
