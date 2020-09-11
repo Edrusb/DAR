3bfdc6a6e6eea5fdbb52adf67c22b613d2ac7d86
Denis Corbin <dar.linux@free.fr>
2020-09-11 15:56:21 +0200

    getting ready for release 2.6.12

--- src/libdar/libdar5.hpp
+++ src/libdar/libdar5.hpp
@@ -356,7 +356,7 @@ namespace libdar5
 	///  libdar Medium version defined at compilation time
     const U_I LIBDAR_COMPILE_TIME_MEDIUM = 100;
 	///  libdar Minor version defined at compilation time
-    const U_I LIBDAR_COMPILE_TIME_MINOR = 11;
+    const U_I LIBDAR_COMPILE_TIME_MINOR = 12;
 
 	////////////////////////////////////////////////////////////////////////
 	// LIBDAR INITIALIZATION METHODS                                      //
