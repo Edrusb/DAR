0ba732d0626855b2fa88813936281c8d7c0a7c8a
Denis Corbin <dar.linux@free.fr>
2021-03-13 20:59:59 +0100

    null_file missed setting the read/write offset when truncating before that position

--- src/libdar/null_file.hpp
+++ src/libdar/null_file.hpp
@@ -82,7 +82,7 @@ namespace libdar
 	    set_offset(offset + siz);
 	};
 
-	virtual void inherited_truncate(const infinint & pos) override {};
+	virtual void inherited_truncate(const infinint & pos) override { if(pos < offset) offset = pos; };
 	virtual void inherited_sync_write() override {};
 	virtual void inherited_flush_read() override {};
 	virtual void inherited_terminate() override {};
