7f583b5c7a8b0f3586106920b6253641ba741780
Denis Corbin <dar.linux@free.fr>
2021-04-08 11:20:38 +0200

    updating pointer information on how to contact the author

--- src/libdar/cat_lien.hpp
+++ src/libdar/cat_lien.hpp
@@ -16,7 +16,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
-// to contact the author : http://dar.linux.free.fr/email.html
+// to contact the author, see the AUTHOR file
 /*********************************************************************/
 
     /// \file cat_lien.hpp
