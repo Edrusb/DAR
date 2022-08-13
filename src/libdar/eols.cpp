f5dbf008b1ce9792af935ab3b551e078ea1b7ac5
Denis Corbin <dar.linux@free.fr>
2022-08-13 19:10:57 +0200

    adding a new class "eols" to detect end of lines in files based of a list of possible string
    this is over the requirment of having a \0 as end of line for filter from files, but
    the current status is to detect both \n and \n\r sequences as end of line in that
    context, and we want to keep beeing able to feed libdar by dos and unix text formated files

---
+++ src/libdar/eols.cpp
@@ -0,0 +1,205 @@
+/*********************************************************************/
+// dar - disk archive - a backup/restoration program
+// Copyright (C) 2002-2022 Denis Corbin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// to contact the author, see the AUTHOR file
+/*********************************************************************/
+
+#include "../my_config.h"
+
+extern "C"
+{
+} // end extern "C"
+
+#include "eols.hpp"
+#include "erreurs.hpp"
+
+using namespace std;
+
+namespace libdar
+{
+    eols::eols(const deque<string> & end_sequences)
+    {
+	eols_curs.clear();
+	ref_progression = 0;
+
+	    // checking against empty sequences
+
+	deque<string>::const_iterator it = end_sequences.begin();
+	while(it != end_sequences.end())
+	{
+	    if(it->empty())
+		throw Erange("eols::eols", gettext("Empty string cannot be provided as sequence defining an end of line"));
+	    eols_curs.push_back(in_progress(*it));
+	    ++it;
+	}
+    }
+
+    void eols::reset_detection() const
+    {
+	deque<in_progress>::iterator it = eols_curs.begin();
+	while(it != eols_curs.end())
+	{
+	    it->reset();
+	    ++it;
+	}
+
+	ref_progression = 0;
+    }
+
+    bool eols::eol_reached(char next_read_byte,
+			   U_I & eol_sequence_length,
+			   U_I & after_eol_read_bytes) const
+    {
+	bool new_match = false;
+	bool all_done = false;
+
+	deque<in_progress>::iterator it = eols_curs.begin();
+	while(it != eols_curs.end())
+	{
+	    if(it->match(next_read_byte))
+	    {
+		if(ref_progression < it->progression())
+		    ref_progression = it->progression();
+		new_match = true;
+	    }
+	    ++it;
+	}
+
+	if(!new_match)
+	    all_done = all_bypassed_or_matched();
+	else
+	    all_done = false;
+
+	if(all_done || new_match)
+	{
+	    if(all_done || bypass_or_larger(ref_progression))
+	    {
+		if(find_larger_match(eol_sequence_length, after_eol_read_bytes))
+		{
+		    reset_detection();
+		    return true;
+		}
+		else
+		    throw SRC_BUG;
+	    }
+		//else some sequences remain possible so we continue
+	}
+	    // else no new match but some remain to be evaluated
+
+	return false;
+    }
+
+    bool eols::in_progress::match(char next_read_byte)
+    {
+	if(bypass)
+	{
+	    if(has_matched())
+		++passed;
+	    return false;
+	}
+
+	if(next_to_match == ref.end())
+	    throw SRC_BUG;
+
+	if(*next_to_match == next_read_byte)
+	{
+	    ++next_to_match;
+	    if(next_to_match == ref.end())
+		return true;
+	}
+	else
+	{
+	    if(!larger)
+		reset();
+	    else
+		set_bypass(progression()+1);
+	}
+
+	return false;
+    }
+
+    U_I eols::in_progress::progression() const
+    {
+	return next_to_match - ref.begin();
+    }
+
+    bool eols::in_progress::set_bypass(U_I prog)
+    {
+	if(bypass)
+	    return true;
+
+	if(has_matched() || progression() < prog)
+	{
+	    bypass = true;
+	    passed = 0;
+	}
+
+	return bypass;
+    }
+
+    bool eols::bypass_or_larger(U_I prog) const
+    {
+	bool ret = true;
+
+	deque<in_progress>::iterator it = eols_curs.begin();
+	while(it != eols_curs.end())
+	{
+	    if(! it->set_bypass(prog))
+	    {
+		it->set_larger();
+		ret = false;
+	    }
+	    ++it;
+	}
+
+	return ret;
+    }
+
+    bool eols::all_bypassed_or_matched() const
+    {
+	deque<in_progress>::iterator it = eols_curs.begin();
+
+	while(it != eols_curs.end() && (it->bypass || it->has_matched()))
+	    ++it;
+
+	return it == eols_curs.end();
+    }
+
+    bool eols::find_larger_match(U_I & seq_length, U_I & read_after_eol) const
+    {
+	bool ret = false;
+
+	deque<in_progress>::iterator it = eols_curs.begin();
+
+	while(it != eols_curs.end() && (! it->has_matched() || it->progression() < ref_progression))
+	    ++it;
+
+	if(it != eols_curs.end())
+	{
+	    if(it->progression() > ref_progression)
+		throw SRC_BUG;
+
+	    ret = true;
+	    seq_length = it->ref.size();
+	    read_after_eol = it->passed;
+	}
+
+	return ret;
+    }
+
+} // end of namespace
---
+++ src/libdar/eols.hpp
@@ -0,0 +1,116 @@
+/*********************************************************************/
+// dar - disk archive - a backup/restoration program
+// Copyright (C) 2002-2022 Denis Corbin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// to contact the author, see the AUTHOR file
+/*********************************************************************/
+
+    /// \file eols.hpp
+    /// \brief provide a way to detect the presence of string from a list of string in a byte flow
+    /// \ingroup Private
+    ///
+    /// This class is used by the mask_list class to detect the end of lines from
+    /// a set of several variable length sequences provided by the user
+
+#ifndef EOLS_HPP
+#define EOLS_HPP
+
+#include "../my_config.h"
+
+#include <string>
+#include <deque>
+
+#include "integers.hpp"
+
+namespace libdar
+{
+
+        /// \addtogroup API
+        /// @{
+
+        /// the class eols provide a way to detect the presence of string from a list of string in a byte flow
+
+    class eols
+    {
+    public:
+	eols(const std::deque<std::string> & end_sequences);
+
+	eols(const eols & ref) = delete;
+	eols(eols && ref) noexcept = delete;
+	eols & operator = (const eols & ref) = delete;
+	eols & operator = (eols && ref) = delete;
+	~eols() = default;
+
+	    /// reset the detection to be beginning of each EOL sequence
+	void reset_detection() const;
+
+	    /// check whether we have reach an end of line
+
+	    /// \param[in] next_read_byte the next byte read from the file
+	    /// \param[out] eol_sequence_length if a EOL sequence is matched, the length of the sequence in bytes
+	    /// \param[out] after_eol_read_bytes if a EOL sequence is matched, the amount of byte alread read after that sequence.
+	    /// Important! the bytes read after eol must be re-fed to the eols object
+	    /// \return true if a EOL sequence is matched, false else
+	    /// \note due to the fact the list of sequence can contain sequence that is a substring of another
+	    /// and that whe have to support the list { "\n" , "\n\r" } for backward compatibility, we need
+	    /// to match the longest string. However it may end that a longer string than a one that just matched
+	    /// finally does not match entierely, but to know that we would have read some extra bytes passed
+	    /// the first (shorter but fully matched) sequence: for example with the two strings { "to", "toto" }
+	    /// as EOL, the read sequence "totu" would have to read the 'u' byte to realize that the longest
+	    /// sequence was 'to', thus we read the second 't' (and the 'u') passed the end of line to know that.
+	bool eol_reached(char next_read_byte,
+			 U_I & eol_sequence_length,
+			 U_I & after_eol_read_bytes) const;
+
+    private:
+	class in_progress
+	{
+	public:
+	    in_progress(const std::string & val): ref(val) { reset(); };
+
+		// methods
+	    void reset() { next_to_match = ref.begin(); bypass = false; passed = 0; larger = false; };
+	    bool match(char next_read_byte);
+	    U_I progression() const; ///< returns the number of matching bytes so far
+	    bool set_bypass(U_I prog); ///< set bypass if progression < prog or has_matched()
+	    void set_larger() { larger = true; };
+	    bool has_matched() const { return next_to_match == ref.end(); };
+
+		// fields
+	    const std::string ref;                     ///< ending sequence
+	    std::string::const_iterator next_to_match; ///< next byte to read from that sequence (previously read bytes matched this sequence)
+	    bool bypass;                               ///< when set match() does nothing except counting bytes
+	    U_I passed;                                ///< number of bytes bypassed (read after end of sequence)
+	    bool larger;                               ///< another in_progress has matched we may also match as a larger sequence
+	};
+
+	mutable std::deque<in_progress> eols_curs;
+	mutable U_I ref_progression;
+
+	    /// set bypass flag for all in_progress of eols_curs that progression is less than or equal 'prog'
+
+	    /// \return true if all elements of eols_curs could be bypassed (none has a larger progression)
+	bool bypass_or_larger(U_I prog) const;
+	bool all_bypassed_or_matched() const;
+	bool find_larger_match(U_I & seq_length, U_I & read_after_eol) const;
+    };
+
+        /// @}
+
+} // end of namespace
+
+#endif
