170a914fd8f51a8aab7a40a18c139376bbf8a548
Denis Corbin <dar.linux@free.fr>
2022-09-06 21:44:48 +0200

    avoiding using std::make_shared with libthreadar
    std::make_shared with clang 10.0.1/FreeBSD 12 does
    not tolerate class having a destructor explicitely
    able to throw exception. (libthreadar::barrier, for
    some good reason). Removing the explicit declaration
    from the constructor leads to invocation of terminate()
    in case of exception when compiling with gcc, which is
    not what we want. This we set the std::shared_ptr
    by mean of its reset() method.
    
    By the way, doing so makes sens here as we test the
    result of the object allocation and throw an Ememory
    exception in case of allocation failure.
    
    The previous code, would not have launched a std::bad_alloc
    exception which is not what we want to provide to the user
    (all thrown exception should be child or inherited child of
    class libdar::Egeneric)

--- src/libdar/parallel_tronconneuse.cpp
+++ src/libdar/parallel_tronconneuse.cpp
@@ -91,21 +91,21 @@ namespace libdar
 	{
 	    U_I tmp_bs1, tmp_bs2;
 
-	    scatter = make_shared<ratelier_scatter<crypto_segment> >(get_ratelier_size(num_workers));
+	    scatter.reset(new (nothrow) ratelier_scatter<crypto_segment>(get_ratelier_size(num_workers)));
 	    if(!scatter)
 		throw Ememory("parallel_tronconneuse::parallel_tronconneuse");
 
-	    gather = make_shared<ratelier_gather<crypto_segment> >(get_ratelier_size(num_workers));
+	    gather.reset(new (nothrow) ratelier_gather<crypto_segment>(get_ratelier_size(num_workers)));
 	    if(!gather)
 		throw Ememory("parallel_tronconneuse::parallel_tronconneuse");
 
-	    waiter = make_shared<barrier>(num_workers + 2); // +1 for crypto_reade thread, +1 this thread
+	    waiter.reset(new (nothrow) barrier(num_workers + 2));
 	    if(!waiter)
 		throw Ememory("parallel_tronconneuse::parallel_tronconneuse");
 
 		// tas is created empty
 
-	    tas = make_shared<heap<crypto_segment> >();
+	    tas.reset(new (nothrow) heap<crypto_segment>());
 	    if(!tas)
 		throw Ememory("parallel_tronconneuse::parallel_tronconneuse");
 
