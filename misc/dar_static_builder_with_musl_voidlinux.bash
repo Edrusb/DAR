9f360ecdfa41aff89677a629b71b3ddea22cfdcb
Denis Corbin <dar.linux@free.fr>
2023-01-29 16:28:15 +0100

    adding script to build dar_static on musl/VoidLinux distro

---
+++ misc/dar_static_builder_with_musl_voidlinux.bash
@@ -0,0 +1,226 @@
+#!/bin/bash
+
+
+# cconfig/ompilation/linking related variables
+
+export MAKE_FLAGS="-j 8"
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/lib/pkgconfig:/usr/lib/pkgconfig
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64:/usr/lib
+
+#export LDFLAGS=-L/usr/local/lib
+#export CFLAGS=-I/usr/local/include
+#export CXXFLAGS=${CFLAGS}
+
+# packages version
+
+LIBTHREADAR_VERSION=1.4.0
+LIBRSYNC_VERSION=2.3.2
+LIBGPG_ERROR_VERSION=1.45
+GNUTLS_VERSION=3.7.8
+LIBCURL_VERSION=7.86.0
+LIBP11KIT_VERSION=0.24.1
+LIBSSH2_VERSION=1.10.0
+
+# wget options need for gnutls website that does not provide all chain of trust in its certificate
+GNUTLS_WGET_OPT="--no-check-certificate"
+
+#
+check()
+{
+    # checking we are under Void Linux
+    if [ $(which lsb_release | wc -l) -eq 0 ] ; then
+	echo "missing lsb_release command to check sanity of the environment"
+	exit 1
+    fi
+
+    if [ "$(lsb_release -i | sed -rn -e 's/^Distributor ID:\s+//p')" != "VoidLinux" ] ; then
+	echo "THis script is only expected to work on VoidLinux Distribution"
+	exit 1
+    fi
+
+    # checking for xbps-install
+    if [ "$(which xbps-install | wc -l)" -eq 0 ] ; then
+	echo "Missing xbps-instal command"
+	exit 1
+    fi
+
+    # checking musl libc is the system standard C library
+    if [ $(xbps-query -l | grep musl | wc -l) -le 0 ] ; then
+	echo "Cannot find musl standard C library"
+	echo "static linking with glibc is broken, musl is needed"
+	exit 1
+    fi
+}
+
+requirements()
+{
+
+    #updating xbps db
+    xbps-install -SU -y
+
+    # tools used to build the different packages involved here
+    xbps-install -y gcc make wget pkg-config cmake xz || exit 1
+
+    #direct dependencies of libdar
+    xbps-install -y bzip2-devel e2fsprogs-devel libargon2-devel libgcc-devel libgcrypt-devel liblz4-devel \
+		 liblzma-devel libstdc++-devel libzstd-devel lz4-devel \
+		 lzo-devel musl-devel zlib-devel || exit 1
+
+    # needed to build static flavor of librsync
+    xbps-install -y libb2-devel || exit 1
+
+    # needed to build gnutls
+    xbps-install -y  nettle-devel libtasn1-devel libunistring-devel unbound-devel unbound || exit 1
+
+
+    #needed for libcurl
+    xbps-install -y libssp-devel || exit 1
+
+}
+
+libthreadar()
+{
+    local LIBTHREADAR_PKG=libthreadar-${LIBTHREADAR_VERSION}.tar.gz
+
+    if [ ! -e "${LIBTHREADAR_PKG}" ] ; then wget "https://dar.edrusb.org/libthreadar/Releases/${LIBTHREADAR_PKG}" || exit 1 ; fi
+    tar -xf ${LIBTHREADAR_PKG} || exit 1
+    cd libthreadar-${LIBTHREADAR_VERSION} || exit 1
+    ./configure && make ${MAKE_FLAGS} || exit 1
+    make install
+    cd ..
+    ldconfig
+    rm -rf libthreadar-${LIBTHREADAR_VERSION}
+}
+
+libgpg-error()
+{
+    local LIBGPG_ERROR_PKG=libgpg-error-${LIBGPG_ERROR_VERSION}.tar.bz2
+
+    if [ ! -e "${LIBGPG_ERROR_PKG}" ] ; then wget https://www.gnupg.org/ftp/gcrypt/libgpg-error/${LIBGPG_ERROR_PKG} || exit 1 ; fi
+    tar -xf ${LIBGPG_ERROR_PKG}
+    cd libgpg-error-${LIBGPG_ERROR_VERSION}
+    ./configure --enable-static
+    make ${MAKE_FLAGS}
+    make install
+    cd ..
+    ldconfig
+    rm -rf libgpg-error-${LIBGPG_ERROR_VERSION}
+}
+
+
+librsync()
+{
+    local LIBRSYNC_PKG=v${LIBRSYNC_VERSION}.tar.gz
+
+    if [ ! -e "${LIBRSYNC_PKG}" ] ; then wget "https://github.com/librsync/librsync/archive/refs/tags/${LIBRSYNC_PKG}" || exit 1 ; fi
+    tar -xf ${LIBRSYNC_PKG}
+    cd librsync-${LIBRSYNC_VERSION}
+    cmake .
+    make ${MAKE_FLAGS}
+    make install
+    cmake -DBUILD_SHARED_LIBS=OFF .
+    make ${MAKE_FLAGS}
+    make install
+    cd ..
+    ldconfig
+    rm -rf librsync-${LIBRSYNC_VERSION}
+}
+
+gnutls()
+{
+    local GNUTLS_PKG="gnutls-${GNUTLS_VERSION}.tar.xz"
+    local REPODIR=$(echo ${GNUTLS_VERSION} | sed -rn -e 's/^([0-9]+\.[0-9]+).*/\1/p')
+
+    if [ -z "$REPODIR" ] ; then echo "empty repo dir for guntls, check GNUTLS_VERSION is correct" ; exit 1 ; fi
+    if [ ! -e "${GNUTLS_PKG}" ] ; then wget ${GNUTLS_WGET_OPT} "https://www.gnupg.org/ftp/gcrypt/gnutls/v${REPODIR}/${GNUTLS_PKG}" || exit 1 ; fi
+    tar -xf "${GNUTLS_PKG}"
+    cd "gnutls-${GNUTLS_VERSION}"
+    ./configure --enable-static --without-p11-kit
+    unbound-anchor -a "/etc/unbound/root.key"
+    make ${MAKE_FLAGS}
+    make install
+
+    # need to tweak the gnutls.pc file due to error in dependency order (hogweed relies on nttle, thus nttle must come after hogweed on linker command line)
+    HOGWEED_PC=/usr/lib/pkgconfig/hogweed.pc
+    if [ -e "${HOGWEED_PC}" ] ; then
+	mv "${HOGWEED_PC}" "${HOGWEED_PC}.bak"
+	sed -r -e 's/#\snettle/nettle/' < "${HOGWEED_PC}.bak" > "${HOGWEED_PC}"
+	rm "${HOGWEED_PC}.bak"
+    else
+	echo "${HOGWEED_PC} not found"
+	exit 1
+    fi    
+    cd ..
+    ldconfig
+    rm -rf "gnutls-${GNUTLS_VERSION}"
+}
+
+libssh2()
+{
+    local LIBSSH2_PKG=libssh2-${LIBSSH2_VERSION}.tar.gz
+
+    if [ ! -e "${LIBSSH2_PKG}" ] ; then wget "https://www.libssh2.org/download/${LIBSSH2_PKG}" || exit 1 ; fi
+    tar -xf ${LIBSSH2_PKG}
+    cd libssh2-${LIBSSH2_VERSION}
+    ./configure --enable-shared --without-libssl --with-libz --with-crypto=libgcrypt
+    make ${MAKE_FLAGS}
+    make install
+    cd ..
+    ldconfig
+    rm -rf libssh2-${LIBSSH2_VERSION}
+}
+
+libcurl()
+{
+    local LIBCURL_PKG=curl-${LIBCURL_VERSION}.tar.bz2
+
+    if [ ! -e "${LIBCURL_PKG}" ] ; then wget "https://curl.se/download/${LIBCURL_PKG}" || exit 1 ; fi
+    tar -xf ${LIBCURL_PKG}
+    cd curl-${LIBCURL_VERSION}
+    ./configure --with-gnutls --with-libssh2 --disable-shared
+    make ${MAKE_FLAGS}
+    make install
+    cd ..
+    ldconfig
+    rm -rf curl-${LIBCURL_VERSION}
+}
+
+dar_static()
+{
+    local dar_version=$(ls -ltr dar-*.tar.* | sed -rn -e 's/.*([0-9]+\.[0-9]+\.[0-9]+).*/\1/p' | tail -n 1)
+    local tmp_dir=/tmp
+    
+    rm -rf "dar-${dar_version}"
+    tar -xf "dar-${dar_version}.tar.gz"
+    cd "dar-${dar_version}"
+    ./configure --enable-static --disable-shared\
+		--enable-libz-linking\
+		--enable-libbz2-linking\
+		--enable-liblzo2-linking\
+		--enable-libxz-linking\
+		--enable-libzstd-linking\
+		--enable-liblz4-linking\
+		--enable-libgcrypt-linking\
+		--enable-librsync-linking\
+		--enable-libcurl-linking\
+		--disable-build-html\
+		--disable-gpgme-linking\
+		--enable-threadar\
+		--enable-libargon2-linking\
+		--disable-python-binding\
+		--prefix=/DAR || exit 1
+    make ${MAKE_FLAGS} || exit 1
+    make DESTDIR=${tmp_dir} install-strip || exit 1
+    mv ${tmp_dir}/DAR/bin/dar_static ..
+    rm -rf ${tmp_dir}/DAR
+}
+
+check
+requirements || (echo "Failed setting up requirements" && exit 1)
+libthreadar || (echo "Failed building libthreadar" && exit 1)
+libgpg-error || (echo "Failed building libgpg-error static version" && exit 1)
+librsync || (echo "Failed building librsync" && exit 1)
+libssh2 || (echo "Failed building libssh2" && exit 1)
+gnutls || (echo "Failed building gnutls" && exit 1)
+libcurl || (echo "Failed building libcurl" && exit 1)
+dar_static || (echo "Failed building dar_static" && exit 1)
