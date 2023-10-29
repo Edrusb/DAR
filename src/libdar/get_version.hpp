ebc20c5eb11df7600cdc82ee82da2f5afc0a5fdd
Denis Corbin <dar.linux@free.fr>
2023-10-29 17:44:40 +0100

    updating libdar API version to 7.0.0
    only minor change should take place from 6.x.x , but a cleanup of
    most probably never unused methods will take place, this triggers incrementing
    major number

--- src/libdar/get_version.hpp
+++ src/libdar/get_version.hpp
@@ -51,11 +51,11 @@ namespace libdar
 	/// @{
 
 	///  libdar Major version defined at compilation time
-    constexpr U_I LIBDAR_COMPILE_TIME_MAJOR = 6;
+    constexpr U_I LIBDAR_COMPILE_TIME_MAJOR = 7;
 	///  libdar Medium version defined at compilation time
-    constexpr U_I LIBDAR_COMPILE_TIME_MEDIUM = 7;
+    constexpr U_I LIBDAR_COMPILE_TIME_MEDIUM = 0;
 	///  libdar Minor version defined at compilation time
-    constexpr U_I LIBDAR_COMPILE_TIME_MINOR = 1;
+    constexpr U_I LIBDAR_COMPILE_TIME_MINOR = 0;
 
 	////////////////////////////////////////////////////////////////////////
 	// LIBDAR INITIALIZATION METHODS                                      //
