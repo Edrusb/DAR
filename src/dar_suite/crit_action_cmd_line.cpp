43103210444e8e87204fc8f097b5a6cab33767c9
Denis Corbin <dar.linux@free.fr>
2025-06-12 17:42:05 +0200

    adding a new flag in overwriting policy on CLI for class crit_in_place_is_binary_patch

--- src/dar_suite/crit_action_cmd_line.cpp
+++ src/dar_suite/crit_action_cmd_line.cpp
@@ -486,6 +486,9 @@ static const criterium * criterium_create_from_string(user_interaction &dialog, 
 	    case 'L':
 		ret = new (nothrow) crit_in_place_has_delta_sig();
 		break;
+	    case 'P':
+		ret = new (nothrow) crit_in_place_is_binary_patch();
+		break;
 	    case 'E':
 		ret = new (nothrow) crit_same_inode_data();
 		break;
