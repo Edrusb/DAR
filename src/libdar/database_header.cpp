c0fa8b2af485b89c90461c1fd4e13e0cf728a40d
Denis Corbin <dar.linux@free.fr>
2025-11-02 13:24:05 +0100

    adding crypto feature to class database_header

--- src/libdar/database_header.cpp
+++ src/libdar/database_header.cpp
@@ -68,15 +68,16 @@ namespace libdar
 
 #define HEADER_OPTION_NONE 0x00
 #define HEADER_OPTION_COMPRESSOR 0x01
+#define HEADER_OPTION_CRYPTO 0x02
 #define HEADER_OPTION_EXTENSION 0x80
 	// if EXTENSION bit is set, the option field is two bytes wide
 	// this mechanism can be extended in the future by a second extension bit 0x8080
-	// and so on
+	// and so on forth
 
     class database_header
     {
     public:
-	database_header() { version = database_version; options = HEADER_OPTION_NONE; algo = compression::gzip; compression_level = 9; };
+	database_header() { version = database_version; algo = compression::gzip; compression_level = 9; };
 	database_header(const database_header & ref) = default;
 	database_header(database_header && ref) noexcept = default;
 	database_header & operator = (const database_header & ref) = default;
@@ -88,25 +89,44 @@ namespace libdar
 
 	void set_compression(compression algozip, U_I level);
 
+	void set_crypto(crypto_algo val) { crypto = val; };
+	void set_kdf_hash(hash_algo val) { kdf_hash = val; };
+	void set_kdf_interation(const infinint & val) { kdf_count = val; };
+	void set_crypto_block_size(U_32 val) { crypto_bs = val; };
+	void set_kdf_salt(const std::string & val) { salt = val; };
+
 	U_I get_version() const { return version; };
 	compression get_compression() const { return algo; };
 	U_I get_compression_level() const { return compression_level; };
+	crypto_algo get_crypto_algo() const { return crypto; };
+	hash_algo get_kdf_hash() const { return kdf_hash; };
+	const infinint & get_kdf_interation() const { return kdf_count; };
+	U_32 get_crypto_block_size() const { return crypto_bs; };
+	const std::string & get_salt() const { return salt; };
 
     private:
 	unsigned char version;
-	unsigned char options;
 	compression algo;
 	U_I compression_level;
+	crypto_algo crypto;
+	hash_algo kdf_hash;
+	infinint kdf_count;
+	U_32 crypto_bs;
+	std::string salt;
     };
 
     void database_header::read(generic_file & f)
     {
+	unsigned char options;
+
 	f.read((char *)&version, 1);
 	if(version > database_version)
 	    throw Erange("database_header::read", gettext("The format version of this database is too high for that software version, use a more recent software to read or modify this database"));
 	f.read((char *)&options, 1);
+
 	if((options & HEADER_OPTION_EXTENSION) != 0)
-	    throw Erange("database_header::read",  gettext("Unknown header option in database, aborting\n"));
+	    throw Erange("database_header::read",  gettext("Unknown header option in database, aborting"));
+
 	if((options & HEADER_OPTION_COMPRESSOR) != 0)
 	{
 	    char tmp;
@@ -122,13 +142,56 @@ namespace libdar
 	}
 	else
 	{
-	    algo = compression::gzip; // was the default before choice was available
+	    algo = compression::gzip; // this was the default setting in earlier format
 	    compression_level = 9;
+	}
+
+	if((options & HEADER_OPTION_CRYPTO) != 0)
+	{
+	    char tmp;
+	    infinint itmp;
+
+	    f.read(&tmp, 1);
+	    crypto = char_2_crypto_algo(tmp);
+
+	    f.read(&tmp, 1);
+	    kdf_hash = char_to_hash_algo(tmp);
+
+	    kdf_count.read(f);
+
+	    itmp.read(f);
+	    crypto_bs = 0;
+	    itmp.unstack(crypto_bs);
+	    if(!itmp.is_zero()) // valid infinint but too large value for crypto_bs
+		throw Erange("database_header::read", gettext("Format error in database crypto header, aborting"));
+
+	    itmp.read(f); // salt size
+	    tools_read_string_size(f, salt, itmp);
+	}
+	else
+	{
+	    crypto = crypto_algo::none;
+	    kdf_hash = hash_algo::none;
+	    kdf_count = 0;
+	    crypto_bs = 0;
+	    salt.clear();
 	}
     }
 
     void database_header::write(generic_file & f)
     {
+	unsigned char options = HEADER_OPTION_NONE;
+
+	if(algo != compression::gzip || compression_level != 9)
+	    options |= HEADER_OPTION_COMPRESSOR;
+	else
+	    options &= ~HEADER_OPTION_COMPRESSOR;
+
+	if(crypto != crypto_algo::none)
+	    options |= HEADER_OPTION_CRYPTO;
+	else
+	    options &= ~HEADER_OPTION_CRYPTO;
+
 	f.write((char *)&version, 1);
 	f.write((char *)&options, 1);
 	if((options & HEADER_OPTION_COMPRESSOR) != 0)
@@ -137,16 +200,32 @@ namespace libdar
 	    f.write(&tmp, 1);
 	    infinint(compression_level).dump(f);
 	}
+	if(crypto != crypto_algo::none)
+	{
+	    char tmp;
+	    infinint itmp;
+
+	    tmp = crypto_algo_2_char(crypto);
+	    f.write(&tmp, 1);
+
+	    tmp = hash_algo_to_char(kdf_hash);
+	    f.write(&tmp, 1);
+
+	    kdf_count.dump(f);
+
+	    itmp = crypto_bs;
+	    itmp.dump(f);
+
+	    itmp = salt.size();
+	    itmp.dump(f);
+	    tools_write_string_all(f, salt);
+	}
     }
 
     void database_header::set_compression(compression algozip, U_I level)
     {
 	algo = algozip;
 	compression_level = level;
-	if(algo != compression::gzip || level != 9)
-	    options |= HEADER_OPTION_COMPRESSOR;
-	else
-	    options &= ~HEADER_OPTION_COMPRESSOR;
     }
 
     generic_file *database_header_create(const shared_ptr<user_interaction> & dialog,
@@ -167,7 +246,6 @@ namespace libdar
 
 	try
 	{
-
 	    if(stat(filename.c_str(), &buf) >= 0 && !overwrite)
 		throw Erange("database_header_create", gettext("Cannot create database, file exists"));
 	    tmp = new (nothrow) fichier_local(dialog, filename, gf_write_only, 0666, !overwrite, overwrite, false);
